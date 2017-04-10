792767a561c25b15b042016cd1e804d4b00c0447
Jeff Kealy <jeffkealy@yahoo.com>
2017-04-10 15:50:45 -0500

    added server

---
+++ public/scripts/client.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  console.log("hello");
+  //scroll to top button
+  $("a[href='#top']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    return false;
+  });
+  //scroll appear
+  window.sr = ScrollReveal({reset: true});
+  sr.reveal('.workContainer h1');
+  sr.reveal('.fudr');
+  sr.reveal('.amDS');
+
+
+  sr.reveal('.lifeContainer')
+  //scroll to work div
+  $("#workButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".workContainer").offset().top
+    }, 600);
+});
+})
