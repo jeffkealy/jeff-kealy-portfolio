1d9e06d3e16faa70a5e9353e35e7b2ab3193332b
jeffkealy <jeffkealy@yahoo.com>
2021-04-05 11:47:19 -0500

    added pages 2 and 3

--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const LOCALPORT = 3000;
 const portDecision = process.env.PORT || LOCALPORT;
 
-app.use(express.static('public'));
-
-app.get('/', function(req, res) {
-  res.sendfile(path.join(__dirname, '../public/views/index.html'));
+app.use(express.static("public"));
+app.get("/page2", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/views/page2.html"));
+});
+app.get("/page3", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/views/page3.html"));
+});
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/views/index.html"));
 });
 
-app.listen(portDecision, () => console.log("Listening on port: ", portDecision));
+app.listen(portDecision, () =>
+  console.log("Listening on port: ", portDecision)
+);
